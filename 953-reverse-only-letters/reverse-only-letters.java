1626cbddfbf58fa2857c11293983d56292900fab
Rahul Patra <161321259+Rahul-65-Patra@users.noreply.github.com>
2025-08-26 13:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (56.91%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -2,16 +2,16 @@ class Solution {
     public String reverseOnlyLetters(String s) {
         
         // time complexity--> O(n)
-        
+
         char[] ch = s.toCharArray();  // space complexity--> O(n)
         int i=0;
         int j=s.length()-1;
 
         while(i<j){
-            while(i<j && !Character.isLetter(ch[i])){
-                i++;
+            while(i<j && !Character.isLetter(ch[i])){   //If arr[i] is not a letter, move i forward.
+                i++;                                    // This skips characters like digits, dashes, etc.
             }
-            while(i<j && !Character.isLetter(ch[j])){
+            while(i<j && !Character.isLetter(ch[j])){   // same as, If arr[j] is not a letter, move j backword.
                 j--;
             }
             if(i<j){
@@ -22,6 +22,6 @@ class Solution {
             j--;
           }
         }
-        return new String(ch);
+        return new String(ch);   // modify char array back to a string
     }
 }
